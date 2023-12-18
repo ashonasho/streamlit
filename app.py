af0c94b140ed780bd2fb13555639e964463f4ba9
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-18 20:20:38 +0530

    header changed

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ json_file_path = 'data.json'
 def load_data(file_path):
     if os.path.exists(file_path):
         with open(file_path, 'r') as file:
+
+
             return json.load(file)
+        
     return []
 
 # Function to save data to JSON file
